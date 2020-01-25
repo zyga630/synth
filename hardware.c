72a75fc7cae24602f9391db1bbdabe5027742dfa
Perpe74 <50866120+Perpe74@users.noreply.github.com>
2020-01-25 20:30:14 +0100

    added ports

--- hardware.c
+++ hardware.c
@@ -13,7 +13,7 @@ uint8_t samples[32] = {152, 176, 199, 218, 234, 246, 253, 255, 253,
 
 void Clock_Init(){
 	SystemCoreClockUpdate();
-	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK; 	
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTA_MASK ; 	
 	SIM->SCGC6 |= SIM_SCGC6_DAC0_MASK;
 	SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
 }
@@ -28,7 +28,16 @@ void GPIO_Init(){
 	//inputs
 	PORTB->PCR[SW0] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
 	PORTB->PCR[SW1] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
-	PORTB->PCR[SW2] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK; 
+	PORTB->PCR[SW2] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+	PORTB->PCR[SW3] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+	PORTB->PCR[SW4] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+	PORTB->PCR[SW5] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;	
+	PORTB->PCR[SW6] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+	PORTB->PCR[SW7] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+	PORTA->PCR[SW8] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+	PORTA->PCR[SW9] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+	PORTA->PCR[SW10] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+	PORTA->PCR[SW11] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
 	
 	//DAC
 	PORTB->PCR[DAC] = PORT_PCR_MUX(1);
@@ -36,6 +45,8 @@ void GPIO_Init(){
 	PTB->PDDR |= 1UL<<RED_LED_PORT;
 	PTB->PDDR |= 1UL<<GREEN_LED_PORT;
 	PTB->PDDR |= 1UL<<BLUE_LED_PORT;
+	
+	
 }
 void DAC_Init(){
 	//Enable DAC, Set Reference Voltage
@@ -48,30 +59,22 @@ void PIT_Init(){
 	NVIC_ClearPendingIRQ(PIT_IRQn);
 	NVIC_EnableIRQ(PIT_IRQn);
 
-
 	PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK;	
 	PIT->MCR = 0x00;
 	
 }
 
 void PIT_IRQHandler(){
-	/*switch(state){
-		case 0:
-			LED_On(RED);
-			state = 1;
-		break;
-		case 1:
-			LED_Off(RED);
-			state = 0;
-		break;					
-	}*/
+	
 	if (g == 32) g = 0;
 	
 		outL = 0xFF & (uint8_t)samples[g++];
 		outH = 0;//0x0F & (uint8_t)(output>>8);
-		DAC0->DAT->DATH = outH*sw_on;
-		DAC0->DAT->DATL = outL*sw_on;
-		PIT->CHANNEL[0].TFLG &= PIT_TFLG_TIF_MASK;  //clear interrupt	
+		
+	
+	DAC0->DAT->DATH = outH * sw_on;
+	DAC0->DAT->DATL = outL * sw_on;
+	PIT->CHANNEL[0].TFLG &= PIT_TFLG_TIF_MASK;  //clear interrupt	
 }
 
 void HW_Init(){
