fb01a5822288372eeb2fa5d07ac4f1130dc3ea2b
zyga630 <58404310+zyga630@users.noreply.github.com>
2020-01-27 19:13:46 +0100

    Delete synth.d

--- synth.d
+++
@@ -1,9 +0,0 @@
-.\synth.o: synth.c
-.\synth.o: synth.h
-.\synth.o: C:\Users\Tomasz\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h
-.\synth.o: C:\Users\Tomasz\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0plus.h
-.\synth.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\synth.o: C:\Users\Tomasz\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
-.\synth.o: C:\Users\Tomasz\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
-.\synth.o: C:\Users\Tomasz\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
-.\synth.o: C:\Users\Tomasz\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h
